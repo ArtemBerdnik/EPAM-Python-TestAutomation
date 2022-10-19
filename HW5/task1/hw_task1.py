e21fa452dfae7a3c00673f27217e4bbbe00fa1a8
Artem_Berdnik <artem_berdnik@epam.com>
2022-10-19 11:38:55 +0300

    first part of HW5 is completed

---
+++ HW5/task1/hw_task1.py
@@ -0,0 +1,19 @@
+class KeyValueStorage:
+
+    def __init__(self, data):
+        self.data = data
+        with open(data, 'r') as d:
+            for line in d:
+                key_and_value = line.split("=")
+                if key_and_value[0].isdigit():
+                    raise ValueError('int value can not be a key in dict')
+                setattr(self, key_and_value[0], int(key_and_value[1].rstrip())) if key_and_value[1].rstrip().isdigit() \
+                    else setattr(self, key_and_value[0], key_and_value[1].rstrip())
+
+    def __getitem__(self, attr):
+        return self.__dict__[attr]
+
+
+if __name__ == "__main__":
+    aaa = KeyValueStorage('/Users/Artem_Berdnik/PycharmProjects/EPAM-Python-TestAutomation/HW5/task1/task1.txt')
+    print()
