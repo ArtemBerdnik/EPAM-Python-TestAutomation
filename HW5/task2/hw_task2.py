2c2ec9f520c5603a7a3fa64bb27d5910b5d237b0
Artem_Berdnik <artem_berdnik@epam.com>
2022-10-21 12:19:55 +0300

    added a new functionality to refresh a data within an object. Added a new test to cover that

--- HW5/task2/hw_task2.py
+++ HW5/task2/hw_task2.py
@@ -3,15 +3,21 @@ import sqlite3
 
 class TableData:
     def __init__(self, database_name, table_name):
+        print("Initiate a new TableData object")
+        print(f"Connecting to {database_name}")
+        print(f"Trying to fetch data from {table_name}")
         conn = sqlite3.connect(database_name)
         cursor = conn.cursor()
         cursor.execute(f'SELECT * from {table_name}')
+        self.database_name = database_name
+        self.table_name = table_name
         self.presidents_data = cursor.fetchall()
         self.presidents_names = [pres[0] for pres in self.presidents_data]
         self.len_ = len(self.presidents_names)
         conn.close()
 
     def __iter__(self):
+        self.__init__(self.database_name, self.table_name)
         return self
 
     def __next__(self):
@@ -22,10 +28,32 @@ class TableData:
         return to_return
 
     def __getitem__(self, attr):
+        print(f"Getting item {attr}")
+        self.__init__(self.database_name, self.table_name)
         return tuple(filter(lambda president: attr in president, self.presidents_data))[0]
 
     def __len__(self):
+        print(f"Getting length of an TableData object")
+        self.__init__(self.database_name, self.table_name)
         return self.len_
 
     def __contains__(self, item):
+        print(f"Checking if a TableData object contains '{item}'")
+        self.__init__(self.database_name, self.table_name)
         return item in self.presidents_names
+
+    @staticmethod
+    def update_data_in_table(president_name: str, age_of_president: int, country: str) -> None:
+        conn = sqlite3.connect('HW5/task2/example.sqlite')
+        cursor = conn.cursor()
+        cursor.execute(f"insert into presidents values ('{president_name}', {age_of_president}, '{country}')")
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def delete_data_in_table(table_name: str, field: str, field_value: str) -> None:
+        conn = sqlite3.connect('HW5/task2/example.sqlite')
+        cursor = conn.cursor()
+        cursor.execute(f"DELETE FROM {table_name} where {field} = '{field_value}'")
+        conn.commit()
+        conn.close()
--- HW5/task2/test_task2.py
+++ HW5/task2/test_task2.py
@@ -4,11 +4,27 @@ from hw_task2 import TableData
 
 @pytest.fixture
 def get_db_details():
-    database_name = 'example.sqlite'
+    database_name = 'HW5/task2/example.sqlite'
     table_name = 'presidents'
     return (
         database_name,
         table_name
+    )
+
+
+@pytest.fixture
+def get_db_details_with_updates():
+    database_name = 'HW5/task2/example.sqlite'
+    table_name = 'presidents'
+    president_name = 'Test_President'
+    president_age = 18
+    president_country = 'Mongolia'
+    return (
+        database_name,
+        table_name,
+        president_name,
+        president_age,
+        president_country
     )
 
 
@@ -24,3 +40,24 @@ def test_presidents(get_db_details):
     assert 'Trump' in presidents
     for president in presidents:
         assert president['name'] is not None and president['name'] in ['Big Man Tyrone', 'Trump', 'Yeltsin']
+
+
+def test_updates_in_db(get_db_details_with_updates):
+    (
+        database_name,
+        table_name,
+        president_name,
+        president_age,
+        president_country
+    ) = get_db_details_with_updates
+
+    presidents = TableData(database_name, table_name)
+
+    amount_of_entries_in_db = len(presidents)
+
+    TableData.update_data_in_table(president_name, president_age, president_country)
+
+    assert len(presidents) == amount_of_entries_in_db + 1
+    assert president_name in presidents
+
+    TableData.delete_data_in_table("presidents", "name", president_name)
